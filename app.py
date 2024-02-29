8bba82a5fd9bd4288de7729440d5aeaffac3d865
Muskan Mirza <145493732+MissMuskan@users.noreply.github.com>
2024-03-01 00:50:49 +0430

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 notes = []
-@app.route('/', methods=["POST"])
-def index():
-    note = request.args.get("note")
-    notes.append(note)
-    return render_template("home.html", notes=notes)
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        note_content = request.form.get('note')
+        if note_content:
+            new_note = {
+                'timestamp': datetime.utcnow(),
+                'content': note_content
+            }
+            notes.append(new_note)
+            return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('home.html', notes=notes) 
